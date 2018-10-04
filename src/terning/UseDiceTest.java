f5c2082935d46fe856eef20c08220af4d63bb52f
Marina <marina@mallingnet.dk>
2018-10-04 16:47:40 +0200

    Junit færdiggjort

--- src/terning/UseDiceTest.java
+++ src/terning/UseDiceTest.java
@@ -1,24 +1,48 @@
 package terning;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class UseDiceTest {
+public class UseDiceTest{
 
-    Dice DiceTest = new Dice();
+    Dice diceTest = new Dice();
 
-    @org.junit.Test
+    @Test
     public void roll() {
 
-        int OneCounter = 0;
+        int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, fejl = 0;
 
         for (int i=0; i < 60000; i++) {
-        int tal = DiceTest.roll();
-        if(tal==1)
-            OneCounter++;
-        assertTrue(tal>0  && tal<7);
+
+        int tal = diceTest.roll();
+        assertTrue(tal > 0 && tal < 7);
+
+        switch (tal) {
+
+            case 1: a1++;
+                break;
+            case 2: a2++;
+                break;
+            case 3: a3++;
+                break;
+            case 4: a4++;
+                break;
+            case 5: a5++;
+                break;
+            case 6: a6++;
+                break;
+            default: fejl++;
+
+        }
     }
 
-        System.out.println(OneCounter);
+        System.out.println(a1 + " " + a2 + " " + a3 + " " + a4 + " " + a5 + " " + a6 + " " + fejl);
+
+    }
+
+    @Test
+    public void rollMultiple(){
 
     }
 }
