fc5b5ac081ad7b8cca6784b150f313373b80efd5
Marina <marina@mallingnet.dk>
2018-10-04 17:50:52 +0200

    Rettelse af anden fejl

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -8,7 +8,9 @@ public class Dice {
 	
 	// roll the die n times and print the values
 	public void rollMultiple(int n) {
-		for (int i=1; i<n; i++) {
+		for (int i=1; i<=n; i++) {
+		    //Da den i den forkerte rollMultiple metode kører roll() metoden; så længe "i" er mindre end n, -
+            //dette bliver rettet til at rollMultiple() metoden kører roll() metoden; så længe at "i" er mindre eller lig med n.
 			System.out.print(roll() + " ");
 		}		
 	}
